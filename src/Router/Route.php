0a11d9dbbdd63769aef3d40adb18f9667bdf0965
Chemaclass <chemaclass@outlook.es>
2023-04-10 08:59:22 +0200

    Refactor Route using __callStatic

--- src/Router/Route.php
+++ src/Router/Route.php
@@ -4,111 +4,41 @@ declare(strict_types=1);
 
 namespace Gacela\Router;
 
+/**
+ * @method static get(string $path, object|string $controller, string $action = '__invoke')
+ * @method static head(string $path, object|string $controller, string $action = '__invoke')
+ * @method static connect(string $path, object|string $controller, string $action = '__invoke')
+ * @method static post(string $path, object|string $controller, string $action = '__invoke')
+ * @method static delete(string $path, object|string $controller, string $action = '__invoke')
+ * @method static options(string $path, object|string $controller, string $action = '__invoke')
+ * @method static patch(string $path, object|string $controller, string $action = '__invoke')
+ * @method static put(string $path, object|string $controller, string $action = '__invoke')
+ * @method static trace(string $path, object|string $controller, string $action = '__invoke')
+ */
 final class Route
 {
     /**
-     * @param object|class-string $controller
+     * @psalm-suppress MixedArgument
      */
-    public static function get(
-        string $path,
-        object|string $controller,
-        string $action = '__invoke',
-    ): void {
-        self::runRoute(Request::METHOD_GET, $path, $controller, $action);
+    public static function __callStatic(string $name, array $arguments): void
+    {
+        match ($name) {
+            'head' => self::route(Request::METHOD_HEAD, ...$arguments),
+            'connect' => self::route(Request::METHOD_CONNECT, ...$arguments),
+            'post' => self::route(Request::METHOD_POST, ...$arguments),
+            'delete' => self::route(Request::METHOD_DELETE, ...$arguments),
+            'options' => self::route(Request::METHOD_OPTIONS, ...$arguments),
+            'patch' => self::route(Request::METHOD_PATCH, ...$arguments),
+            'put' => self::route(Request::METHOD_PUT, ...$arguments),
+            'trace' => self::route(Request::METHOD_TRACE, ...$arguments),
+            default => self::route(Request::METHOD_GET, ...$arguments),
+        };
     }
 
     /**
      * @param object|class-string $controller
      */
-    public static function head(
-        string $path,
-        object|string $controller,
-        string $action = '__invoke',
-    ): void {
-        self::runRoute(Request::METHOD_HEAD, $path, $controller, $action);
-    }
-
-    /**
-     * @param object|class-string $controller
-     */
-    public static function connect(
-        string $path,
-        object|string $controller,
-        string $action = '__invoke',
-    ): void {
-        self::runRoute(Request::METHOD_CONNECT, $path, $controller, $action);
-    }
-
-    /**
-     * @param object|class-string $controller
-     */
-    public static function delete(
-        string $path,
-        object|string $controller,
-        string $action = '__invoke',
-    ): void {
-        self::runRoute(Request::METHOD_DELETE, $path, $controller, $action);
-    }
-
-    /**
-     * @param object|class-string $controller
-     */
-    public static function options(
-        string $path,
-        object|string $controller,
-        string $action = '__invoke',
-    ): void {
-        self::runRoute(Request::METHOD_OPTIONS, $path, $controller, $action);
-    }
-
-    /**
-     * @param object|class-string $controller
-     */
-    public static function patch(
-        string $path,
-        object|string $controller,
-        string $action = '__invoke',
-    ): void {
-        self::runRoute(Request::METHOD_PATCH, $path, $controller, $action);
-    }
-
-    /**
-     * @param object|class-string $controller
-     */
-    public static function post(
-        string $path,
-        object|string $controller,
-        string $action = '__invoke',
-    ): void {
-        self::runRoute(Request::METHOD_POST, $path, $controller, $action);
-    }
-
-    /**
-     * @param object|class-string $controller
-     */
-    public static function put(
-        string $path,
-        object|string $controller,
-        string $action = '__invoke',
-    ): void {
-        self::runRoute(Request::METHOD_PUT, $path, $controller, $action);
-    }
-
-    /**
-     * @param object|class-string $controller
-     */
-    public static function trace(
-        string $path,
-        object|string $controller,
-        string $action = '__invoke',
-    ): void {
-        self::runRoute(Request::METHOD_TRACE, $path, $controller, $action);
-    }
-
-    /**
-     * @param object|class-string $controller
-     */
-    private static function runRoute(
+    private static function route(
         string $method,
         string $path,
         object|string $controller,
