f64162c59e6692bbfe295a7d7515308fe8673ef2
Chemaclass <chemaclass@outlook.es>
2023-04-21 19:59:06 +0200

    Create JsonResponse

---
+++ src/Router/Entities/JsonResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gacela\Router\Entities;
+
+final class JsonResponse
+{
+    public function __construct(
+        private array $json,
+    ) {
+    }
+
+    public function __toString(): string
+    {
+        //        header('Content-Type: application/json');
+
+        return json_encode($this->json, JSON_THROW_ON_ERROR);
+    }
+}
---
+++ tests/Feature/Router/RouterJsonResponseTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GacelaTest\Feature\Router;
+
+use Gacela\Router\Entities\JsonResponse;
+use Gacela\Router\Entities\Request;
+use Gacela\Router\Router;
+use Gacela\Router\Routes;
+use GacelaTest\Feature\HeaderTestCase;
+
+final class RouterJsonResponseTest extends HeaderTestCase
+{
+    public function test_simple_redirect(): void
+    {
+        $_SERVER['REQUEST_URI'] = 'https://example.org/optional/uri';
+        $_SERVER['REQUEST_METHOD'] = Request::METHOD_GET;
+
+        Router::configure(static function (Routes $routes): void {
+            $routes->get('optional/uri', static fn () => new JsonResponse([
+                'key' => 'value',
+            ]));
+        });
+
+        $this->expectOutputString(json_encode(['key' => 'value']));
+
+        //        self::assertSame([
+        //            [
+        //                'header' => 'Content-Type: application/json',
+        //                'replace' => true,
+        //                'response_code' => 0,
+        //            ],
+        //        ], $this->headers());
+    }
+}
