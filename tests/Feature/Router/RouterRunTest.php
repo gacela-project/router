b3687a60c5807f5a9b93681f211af851311ce5d4
Chemaclass <chemaclass@outlook.es>
2023-04-25 10:04:25 +0200

    Split Router::configure() into different object methods

---
+++ tests/Feature/Router/RouterRunTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GacelaTest\Feature\Router;
+
+use Gacela\Router\Entities\Request;
+use Gacela\Router\Entities\Response;
+use Gacela\Router\Router;
+use Gacela\Router\Routes;
+use GacelaTest\Feature\HeaderTestCase;
+
+final class RouterRunTest extends HeaderTestCase
+{
+    public function test_run_multiple_route_collections(): void
+    {
+        $_SERVER['REQUEST_URI'] = 'https://example.org/uri-2';
+        $_SERVER['REQUEST_METHOD'] = Request::METHOD_GET;
+
+        $router = Router::create();
+
+        $router->addRoutes(static function (Routes $routes): void {
+            $routes->get('uri-1', static fn () => new Response('first body'));
+        });
+
+        $router->addRoutes(static function (Routes $routes): void {
+            $routes->get('uri-2', static fn () => new Response('second body'));
+        });
+
+        $router->run();
+
+        $this->expectOutputString('second body');
+
+        self::assertSame([], $this->headers());
+    }
+
+    public function test_run_same_multiple_route_collections_then_first_with_priority(): void
+    {
+        $_SERVER['REQUEST_URI'] = 'https://example.org/uri';
+        $_SERVER['REQUEST_METHOD'] = Request::METHOD_GET;
+
+        $router = Router::create();
+
+        $router->addRoutes(static function (Routes $routes): void {
+            $routes->get('uri', static fn () => new Response('first body'));
+        });
+
+        $router->addRoutes(static function (Routes $routes): void {
+            $routes->get('uri', static fn () => new Response('second body'));
+        });
+
+        $router->run();
+
+        $this->expectOutputString('first body');
+
+        self::assertSame([], $this->headers());
+    }
+}
